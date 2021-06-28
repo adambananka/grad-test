f239aec1755f276bffe403ec5e62d0bf4118b73c
Adam Bananka <addame121@gmail.com>
2021-06-28 06:51:29 +0200

    reset of the activity on screen rotate fixed

--- sget-mobile/app/src/main/java/cz/bald/sget/mobile/ui/setup/SetupActivity.kt
+++ sget-mobile/app/src/main/java/cz/bald/sget/mobile/ui/setup/SetupActivity.kt
@@ -24,9 +24,11 @@ class SetupActivity : AppCompatActivity(), FragmentChangeListener,
     super.onCreate(savedInstanceState)
     setContentView(R.layout.activity_setup)
 
-    val setting = TestSetting(TestType.MATURITA, Language.SLOVAK, CzechSubject.CZECH, 0)
-    val fragment = StartFragment(setting)
-    swapFragment(fragment, false)
+    if (savedInstanceState == null) {
+      val setting = TestSetting(TestType.MATURITA, Language.SLOVAK, CzechSubject.CZECH, 0)
+      val fragment = StartFragment(setting)
+      swapFragment(fragment, false)
+    }
   }
 
   override fun swapFragment(newFragment: Fragment, stack: Boolean) {
