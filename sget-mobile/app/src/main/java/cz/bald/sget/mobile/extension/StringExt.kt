daf4955c8d42b80c681f3d844f64a0d3bc1b55aa
Adam Bananka <addame121@gmail.com>
2021-06-30 20:07:48 +0200

    listing of test results added

---
+++ sget-mobile/app/src/main/java/cz/bald/sget/mobile/extension/StringExt.kt
@@ -0,0 +1,15 @@
+package cz.bald.sget.mobile.extension
+
+import cz.bald.sget.mobile.model.TestSetting
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+fun Date.toPrintable(): String {
+  val dateFormat = SimpleDateFormat("dd.MM.yyyy hh:mm", Locale.getDefault())
+  return dateFormat.format(this)
+}
+
+fun TestSetting.toPrintable(): String {
+  return this.type.name + " / " + this.language + " / " + this.subject + " / " + this.year
+}
